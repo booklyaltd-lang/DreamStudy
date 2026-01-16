7038d63e8740ffa0aa58bfded1517ecf46b632d0
booklyaltd-lang <booklyaltd@gmail.com>
2026-01-16 09:25:44 +0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { SiteSettingsProvider } from './contexts/SiteSettingsContext';
+import { AuthProvider } from './contexts/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SiteSettingsProvider>
-      <App />
-    </SiteSettingsProvider>
+    <AuthProvider>
+      <SiteSettingsProvider>
+        <App />
+      </SiteSettingsProvider>
+    </AuthProvider>
   </StrictMode>
 );
