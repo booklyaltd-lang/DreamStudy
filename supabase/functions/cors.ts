db9e88d3a32d42324174f80e44eb6bc872ff6c18
Pavel Slesarev <booklyaltd@gmail.com>
2026-01-25 10:18:30 +0400

    Deleted cors.ts

--- supabase/functions/cors.ts
+++
@@ -1,5 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization,content-type,cloudpayments-notification-type',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-}
